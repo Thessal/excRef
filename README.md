63918292093ad66f33690c59673be02160e4c8d8
Jongkook Choi <jongkook90@gmail.com>
2021-01-21 18:16:49 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # excRef
-Automatic reference tracking
+Automatic reference tracking.
+
+Converts bibliography into summarized and translated text
+
+## Usage
+Check out main.py
+```
+bibliography = {
+    "some_index_1": "Some title, Author, Journal, Kewords,... ",
+    "some_index_2": "Some title, Author, Journal, Kewords,... "
+    "some_index_3": "Some title, Author, Journal, Kewords,... "
+}
+gather(bibliography, target_lang="ko")
+```
+
+## Dependency
+* Python 3.9.0 
+
+
+* scholarly 
+* title2bib
+* scidownl
+* bibtexparser
+* textract
+* pysummarization
+* googletrans==4.0.0-rc1
+
+```
+pip install scholarly title2bib scidownl bibtexparser textract pysummarization googletrans==4.0.0-rc1
+```
---
+++ main.py
@@ -0,0 +1,68 @@
+# from scihub2pdf.download import  get_bib_from_title
+from title2bib.crossref import get_bib_from_title
+from scholarly import scholarly
+from scidownl.scihub import *
+import bibtexparser
+import os
+import textract
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+from googletrans import Translator  # pip install googletrans==4.0.0-rc1
+
+out_dir = lambda x: os.path.join('./pdf/', x)
+
+
+def _get_pdf(k, title):
+    # Skip if exists
+    if os.path.isdir(out_dir(k)) and os.listdir(out_dir(k)):
+        return os.path.join(out_dir(k), os.listdir(out_dir(k))[0])
+    # Normalize title
+    google_search = scholarly.search_pubs(title)
+    google_result = next(google_search)
+    title = google_result['bib']['title'] + ' ' + (' '.join(google_result['bib']['author']))
+    # Get DOI
+    print(title)
+    found, bib_string = get_bib_from_title(title)
+    # Download
+    if found:
+        bib = bibtexparser.loads(bib_string).entries
+        if bib and ("doi" in bib[0]) and (bib[0]['ENTRYTYPE'] == 'article'):
+            doi = bib[0]["doi"]
+            SciHub(doi, out_dir(k)).download(choose_scihub_url_index=3)
+            pdf = os.path.join(out_dir(k), os.listdir(out_dir(k))[0]) if os.listdir(out_dir(k)) else None
+            return pdf
+        else:
+            print(bib)
+            print("\tAbsent DOI")
+    return None
+
+
+def _set_summarizer():
+    auto_abstractor = AutoAbstractor()
+    auto_abstractor.tokenizable_doc = SimpleTokenizer()
+    auto_abstractor.delimiter_list = [". "]  # [".", "\n"]
+    abstractable_doc = TopNRankAbstractor()
+    abstractable_doc.set_top_n(10)
+    return lambda text: auto_abstractor.summarize(text, abstractable_doc)["summarize_result"]
+
+
+def gather(bibliography, target_lang):
+    summarize = _set_summarizer()
+    translator = Translator()
+
+    pdfs = {k: _get_pdf(k, v) for k, v in bibliography.items()}
+    texts = {k: str(textract.process(pdf), 'utf-8').replace('\n', ' ') for k, pdf in pdfs.items() if pdf}
+    summaries = {k: '\n'.join(summarize(text)) for k, text in texts.items()}
+    translates = {k: translator.translate(text, src='en', dest=target_lang).text for k, text in summaries.items()}
+
+    for k, text in translates.items():
+        print("====")
+        print(k)
+        print(text)
+        print("====")
+
+bibliography = {
+    "ch_1_1": "Plasmonic structure"
+}
+gather(bibliography, target_lang="ko")
